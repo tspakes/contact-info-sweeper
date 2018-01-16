7d7b94806caa37f37c21d610ba5d93f088e0ad8e
Tristan Spakes <tspakes@vols.utk.edu>
2018-01-15 21:02:17 -0500

    Adding bad phone number detection and print to sweeper

--- sweeper.py
+++ sweeper.py
@@ -1,18 +1,46 @@
-
 def sweep(inputFileName):
-	file = open(inputFileName, r)
-	outFile = open(badEntries, w)
+	f = open(inputFileName, "r")
+	outFile = open("badEntries.txt", "w")
 	charCount = 1	
 	invalidEntry = False
+	entry = ""
+	currentChar = "-1"
+	writtenTo = False
 
 	while(True):
-		currentChar = read(1)
+		currentChar = f.read(1)
+		if(currentChar == ""):
+			break
+		cVal = ord(currentChar)
+
 		if(currentChar == " " or currentChar == "\n"):
 			continue
-		elif(currentChar == 4 or currentChar == 8
+		elif(charCount == 4 or charCount == 8):
+			if(currentChar != "-"):
+				invalidEntry = True
+		#elif(cVal < ord('A') or cVal > ord('Z') and cVal < ord('a') or cVal > ord('z') and cVal < ord('0') or cVal > ord('9')):
+		elif(currentChar == ','):
+			if(invalidEntry == True):
+				if(writtenTo == True):
+					outFile.write(", ")
+				outFile.write(entry)
+				writtenTo = True
+			entry = ""
+			charCount = 1
+			invalidEntry = False
+			continue #Begins a new entry
+		elif(cVal < ord('0') or cVal > ord('9')):
+			invalidEntry = True
+		entry += currentChar #Adding currentChar to the current entry
 		charCount += 1
-	file.close()	
+	
+	if(entry != "" and invalidEntry == True): #Writing last invalid entry
+		if(writtenTo == True):
+			outFile.write(", ")
+		outFile.write(entry)
+
+	f.close()	
 	outFile.close()
 	return
 
-
+sweep("phoneNumbers.txt")
